f5b65aefd68a5d4d984ffc81b9b56bb4810ae2b8
Pu2f <chatchanok.nt@gmail.com>
2026-02-14 21:13:54 +0700

    Revert "[WIP] Update code and files for walking history storage"

--- chess_core/board.py
+++ chess_core/board.py
@@ -1,6 +1,5 @@
 from chess_core.square import *
 from chess_core.pieces import *
-from chess_core.move_history import Move, MoveHistory
 
 def board_range(num): #used to verify movements of most pieces
 
@@ -18,7 +17,6 @@ def board_range(num): #used to verify movements of most pieces
 class Board:
 
     board = []
-    move_history = MoveHistory()
 
     def create_board():
 
@@ -66,14 +64,6 @@ class Board:
             for y in x:
                 print(y, end=' ')
             print()
-    
-    def get_move_history():
-        """Return the move history object"""
-        return Board.move_history
-    
-    def print_move_history() -> None:
-        """Print all moves made in the game"""
-        print(Board.move_history)
 
 def has_path_rook(start: Square, end: Square) -> bool: #check if path is clear for rook
 
